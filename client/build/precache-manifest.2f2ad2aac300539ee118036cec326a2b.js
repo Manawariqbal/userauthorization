45a3708d4bc932945d46fee82e3d2915509e830c
Manawariqbal <mmiqbalmanawar@gmail.com>
2021-09-10 17:31:52 +0530

    first

---
+++ client/build/precache-manifest.2f2ad2aac300539ee118036cec326a2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3fe47ba69cace23a8cdf8d0fde832df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ed47e262950052e7168",
+    "url": "/static/css/main.3fdd4ed6.chunk.css"
+  },
+  {
+    "revision": "f90cf4389c983909adb6",
+    "url": "/static/js/2.a9ab6bd5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a9ab6bd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed47e262950052e7168",
+    "url": "/static/js/main.7aee74a6.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
---
+++ client/src/components/body/home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './home.css'
+
+function Home() {
+    return (
+        <div className="home_page">
+            <h2>Hello everyone!</h2>
+            <p>
+                I created this site as  project for students dashboard you can follow me on github to see my projects
+            </p>
+            <a href="https://github.com/Manawariqbal" target="_blank" 
+            rel="noopener noreferrer">My github profile</a>
+
+
+        </div>
+    )
+}
+
+export default Home
